8195769a3981d0a7e524ac7c26e2f5da9ea394e1
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2026-05-16 16:39:11 +0100

    static

--- src/static/Footer.jsx
+++ src/static/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Footer = () => {
   return (
     <div>
-      app
-      
+      footer
     </div>
   )
 }
 
-export default App
+export default Footer
